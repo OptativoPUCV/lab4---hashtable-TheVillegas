33f960ebc3945eeb0ecbffc6a26fcdfe860c8c33
Replit user <>
2023-04-21 03:32:00 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -80,6 +80,7 @@ void eraseMap(HashMap * map,  char * key) {
         if(strcmp(key,map->buckets[i]->key)==0)
         {
           map->buckets[i]->key = NULL;
+          break;
         }
       } 
     }
